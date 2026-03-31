d9e06c2543ed34d84c8920e247d27e6071d3a094
Nilesh Koshta <nileshkoshta008@gmail.com>
2026-04-01 01:30:14 +0530

    Update API config in client/src/api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API = "http://localhost:3000";
+const API_URL = "http://localhost:3000";
 
-export const addPoints = (data) => axios.post(`${API}/addPoints`, data);
-export const getLeaderboard = () => axios.get(`${API}/leaderboard`);
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+// Add token to requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const signup = (data) => api.post('/signup', data);
+export const login = (data) => api.post('/login', data);
+export const addPoints = (data) => api.post('/addPoints', data);
+export const getLeaderboard = () => api.get('/leaderboard');
+
+export default api;
